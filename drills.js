98639f856980b23d568bacbf02b74ea8254177c3
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-05 08:39:18 -0700

    Binary class complete

---
+++ drills.js
@@ -0,0 +1,18 @@
+// BST
+// Given the following data 3,1,4,6,9,2,5,7. If you were to insert
+// this into an empty Binary Search Tree, how would the tree look like?
+//(Draw the tree, no coding needed here)
+
+//                         3
+//                 1             4
+//                      2    5       6
+//                                       9
+//                                   7
+//
+
+// Remote Root
+
+//                        4
+//                   1         6
+//                       2   5     9
+//                             7           
